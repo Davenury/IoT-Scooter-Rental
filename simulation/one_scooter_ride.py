e43d7f79fb77045881bd510e0e8c184ec79e38c3
Dawid Dębowski <dawid199960@gmail.com>
2021-01-28 11:24:50 +0100

    One scooter ride update

--- simulation/one_scooter_ride.py
+++ simulation/one_scooter_ride.py
@@ -7,10 +7,12 @@ from simulation.Simulation import simulate_one_ride
 if __name__ == "__main__":
     scooter = ScooterFactory.create(
         battery_model="Dualthron Thunder",
-        cokolwiek="cokolwiek2",
+        vehicle_type=1,
+        mac="5b:cd:ea:64:12:da",
         y=19.913833776717745,
         x=50.067602018072364,
         battery_level=80,
+        battery_temp=30,
         set_custom_battery_functions=False
     )
     simulate_one_ride(scooter, mode="print", next_point=[19.936798813465444, 50.06204171628422])
